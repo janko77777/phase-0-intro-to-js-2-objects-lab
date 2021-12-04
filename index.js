8705a80eaf8b96d44c06eacbd84c5310a9c30c56
Steven <steven.jankowski1@gmail.com>
2021-12-04 02:12:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+    name: "Steven J",
+    streetAddress: "100 Browns Lane, Knowle, Solihull, UK",
+  };
+
+  function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+      ...obj,
+      [key]: value,
+    };
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+  
+    return obj;
+  }
+
+  function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+    delete newObj[key];
+  
+    return newObj;
+  }
+
+
+  function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+
+    return obj;
+  }
